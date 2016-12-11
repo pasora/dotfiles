86de819bfd1dce4175d455b795d1733fd2919d09
Masahiko Hara <pasora@sfc.wide.ad.jp>
2016-12-11 17:58:03 +0900

    termux

---
+++ setup_vim_termux.sh
@@ -0,0 +1,13 @@
+#!/data/data/com.termux/files/usr/bin/sh
+
+cp -f .vimrc ~/.vimrc
+mkdir -p ~/.vim/rc
+cp -f dein.toml ~/.vim/rc/dein.toml
+cp -f dein_lazy.toml ~/.vim/rc/dein_lazy.toml
+cp -f .latexmkrc ~/.latexmkrc
+mkdir -p ~/.vim/ftplugin
+cp -f tex_quickrun.vim ~/.vim/ftplugin/tex_quickrun.vim
+
+vim
+
+exit 0
